5df9832aa647726331d069358d657eade8d27369
aengincoskun <aengincoskun@gmail.com>
2024-03-14 01:35:59 +0300

    MakerController & MakerService

--- src/main/java/com/bankingeen/backofficeservice/controller/AdminController.java
+++ src/main/java/com/bankingeen/backofficeservice/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.bankingeen.backofficeservice.controller;
 
 import com.bankingeen.backofficeservice.model.contract.admin.*;
 import com.bankingeen.backofficeservice.service.AdminService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,18 +17,21 @@ public class AdminController {
         this.adminService = adminService;
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping(value = "/table/list/v1")
     public ListTableResponse getTables() {
 
         return adminService.getTables();
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping(value = "/table/column/list/v1")
     public ListTableColumnResponse getTableColumns(ListTableColumnRequest request) {
 
         return adminService.getTableColumns(request);
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping(value = "/scenario/create/v1")
     public ScenarioCreateResponse scenarioCreate(ScenarioCreateRequest request) {
 
